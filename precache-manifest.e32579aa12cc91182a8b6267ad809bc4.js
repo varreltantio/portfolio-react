8497ee14e55e04682969beffad89cb04fd24192a
VarrelTantio <snetid90@gmail.com>
2020-07-21 10:26:01 +0700

    Updates

---
+++ precache-manifest.e32579aa12cc91182a8b6267ad809bc4.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44baaf68d832856e22015f20c674dbb1",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "bc5b3af267adedfabc09",
+    "url": "/portfolio-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f9a0602b8576959fb9a2",
+    "url": "/portfolio-react/static/css/main.f7ecb766.chunk.css"
+  },
+  {
+    "revision": "bc5b3af267adedfabc09",
+    "url": "/portfolio-react/static/js/2.cd2b0998.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.cd2b0998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a0602b8576959fb9a2",
+    "url": "/portfolio-react/static/js/main.5539b26f.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "9ee5a75b2988f3660289cc751060076b",
+    "url": "/portfolio-react/static/media/artificial-intelligence.9ee5a75b.png"
+  },
+  {
+    "revision": "49c25ea49e53026bd907ae5ca03e12fb",
+    "url": "/portfolio-react/static/media/blog-1.49c25ea4.jpg"
+  },
+  {
+    "revision": "56ea1c8fcd5d2184d6af0d8774213974",
+    "url": "/portfolio-react/static/media/blog-2.56ea1c8f.jpg"
+  },
+  {
+    "revision": "3d86740d5b2807800b235f5814e188e2",
+    "url": "/portfolio-react/static/media/blog-3.3d86740d.jpg"
+  },
+  {
+    "revision": "ae282de4122a258caeefe5710bedfc03",
+    "url": "/portfolio-react/static/media/blog-4.ae282de4.jpg"
+  },
+  {
+    "revision": "569d52ecc92564c168572858eb5181a7",
+    "url": "/portfolio-react/static/media/blog-5.569d52ec.jpg"
+  },
+  {
+    "revision": "caab4d4cd4febee848301f0062f25604",
+    "url": "/portfolio-react/static/media/blog-6.caab4d4c.jpg"
+  },
+  {
+    "revision": "5720a2b2341e66d42398a866090d6ab5",
+    "url": "/portfolio-react/static/media/brand.5720a2b2.png"
+  },
+  {
+    "revision": "7fa89afec5f2715fe4e9cbd993b03703",
+    "url": "/portfolio-react/static/media/css.7fa89afe.png"
+  },
+  {
+    "revision": "78df2fb1b0504ddc6f4d1e9ff36d41ba",
+    "url": "/portfolio-react/static/media/img1.78df2fb1.png"
+  },
+  {
+    "revision": "ac316b78f604251f06811084fd3536eb",
+    "url": "/portfolio-react/static/media/img2.ac316b78.png"
+  },
+  {
+    "revision": "f5a1b615c0e27984dc584b3dfda433de",
+    "url": "/portfolio-react/static/media/img3.f5a1b615.png"
+  },
+  {
+    "revision": "e5bce38c5e73293b61455e9c6aac5b5e",
+    "url": "/portfolio-react/static/media/info-1.e5bce38c.jpg"
+  },
+  {
+    "revision": "a2d9038dcfe9e3e81373b6bf0cac81ba",
+    "url": "/portfolio-react/static/media/info-2.a2d9038d.jpg"
+  },
+  {
+    "revision": "24e2d15822d9fa65592d054a46382ab9",
+    "url": "/portfolio-react/static/media/info-3.24e2d158.jpg"
+  },
+  {
+    "revision": "8c73946af29651efcbfea2c66e1ee49d",
+    "url": "/portfolio-react/static/media/jb3.8c73946a.jpg"
+  },
+  {
+    "revision": "ecf3c9cbc6baffa87c1b7a35692a77bc",
+    "url": "/portfolio-react/static/media/jb4.ecf3c9cb.jpg"
+  },
+  {
+    "revision": "663d1116c3fa355b6c0f1c6dc7c2e1b7",
+    "url": "/portfolio-react/static/media/js.663d1116.png"
+  },
+  {
+    "revision": "6a5ddb5c133bde42178ad06e6072c6c6",
+    "url": "/portfolio-react/static/media/php.6a5ddb5c.png"
+  },
+  {
+    "revision": "566f41655a30760dddccf2ddad836d2e",
+    "url": "/portfolio-react/static/media/project-1.566f4165.jpg"
+  },
+  {
+    "revision": "ae1153cab46969a7c3ae7bc27643aef6",
+    "url": "/portfolio-react/static/media/project-2.ae1153ca.jpg"
+  },
+  {
+    "revision": "27db8a8ffda4c1f5e609d97543f9aa96",
+    "url": "/portfolio-react/static/media/project-3.27db8a8f.jpg"
+  },
+  {
+    "revision": "063b268c2da211b441d0b076c7d85d43",
+    "url": "/portfolio-react/static/media/project-4.063b268c.jpg"
+  },
+  {
+    "revision": "e4ca46527a4b4d8e4c27d74e2d76ef71",
+    "url": "/portfolio-react/static/media/project-5.e4ca4652.jpg"
+  },
+  {
+    "revision": "7af863c0ac2ba93d2184b688cf480a33",
+    "url": "/portfolio-react/static/media/vector-web.7af863c0.jpg"
+  }
+]);
